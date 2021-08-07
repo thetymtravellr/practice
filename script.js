4874c295ea74855fe0aae48014886597b632476c
Robiul Hasan <muhammadrobiul502@gmail.com>
2021-08-07 12:15:08 +0600

    created

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const menuBtn = document.querySelector(".toggle");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+});
